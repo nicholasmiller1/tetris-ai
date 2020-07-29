99cbda93ddac06caf98990bdd8c0f5e1fbfc3d3a
Nicholas Miller <nicholascmiller123@gmail.com>
2020-07-29 13:43:57 -0400

    Used global variables to set screen and stage sizes

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,27 @@ import processing.core.PApplet;
 
 public class Main extends PApplet {
 
+    private static final int SCREEN_WIDTH = 800;
+    private static final int SCREEN_HEIGHT = 800;
+
+    private static final int STAGE_WIDTH = 400;
+    private static final int STAGE_HEIGHT = 600;
+
     @Override
     public void settings() {
-        size(500, 500);
+        size(SCREEN_WIDTH, SCREEN_HEIGHT);
+    }
+
+    @Override
+    public void setup() {
+        background(255);
     }
 
     @Override
     public void draw() {
-        ellipse(1,1,1,1);
+        rect(SCREEN_WIDTH/2 - STAGE_WIDTH/2, SCREEN_HEIGHT/2 - STAGE_HEIGHT/2, STAGE_WIDTH, STAGE_HEIGHT);
+
+
     }
 
     public static void main(String[] args) {
